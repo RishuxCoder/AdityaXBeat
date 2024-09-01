e4139ac6fb09151d7182c864e03741c517fafef4
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-01 13:11:24 +0530

    Update queue.py

--- SHUKLAMUSIC/utils/inline/queue.py
+++ SHUKLAMUSIC/utils/inline/queue.py
@@ -116,7 +116,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="▪️ ᴍᴏʀᴇ ▪️",
-                url="https://t.me/Inflexupdate",
+                url="https://t.me/ur_rishu_143",
             ),
         ],
     ]
