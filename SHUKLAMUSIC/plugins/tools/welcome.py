0055218b4b75b4413abfcce591b7c0a54fe9d59c
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-06 09:42:35 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -253,10 +253,10 @@ async def greet_new_member(_, member: ChatMemberUpdated):
 **✰────✦ ᴡᴇʟᴄᴏᴍᴇ ✦────✰**
 
 ▰▰▰▰▰▰▰▰▰▰▰▰▰
-**❥︎ ɴᴀᴍᴇ »** {user.mention}
-**❥︎ ɪᴅ »** `{user.id}`
-**❥︎ ᴜ_ɴᴀᴍᴇ »** @{user.username}
-**❥︎ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs »** {count}
+**➥ ɴᴀᴍᴇ »** {user.mention}
+**➥ ɪᴅ »** `{user.id}`
+**➥ ᴜ_ɴᴀᴍᴇ »** @{user.username}
+**➥ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs »** {count}
 ▰▰▰▰▰▰▰▰▰▰▰▰▰
 
 **✰─────✧❅✦❅✧─────✰**
